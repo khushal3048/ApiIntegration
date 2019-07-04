74c32e94dae23439b10a8aa23e9f631a9dee6468
Khushal Kakadiya <khushalkakadiya3048@gmail.com>
2019-07-04 17:10:55 -0400

    Display In LOGCAT

--- app/src/main/java/com/example/apiintegration/Products.java
+++ app/src/main/java/com/example/apiintegration/Products.java
@@ -1,12 +1,24 @@
 package com.example.apiintegration;
 
-public class Products {
+import android.os.Parcel;
+import android.os.Parcelable;
 
-    String pimg, pname;
+public class Products implements Parcelable {
+
+    String pimg, pname, brand, desc;
+    long price;
 
     public Products(String pimg, String pname) {
         this.pimg = pimg;
         this.pname = pname;
+    }
+
+    public Products(String pimg, String pname, String brand, String desc, long price) {
+        this.pimg = pimg;
+        this.pname = pname;
+        this.brand = brand;
+        this.desc = desc;
+        this.price = price;
     }
 
     public String getPimg() {
@@ -24,4 +36,29 @@ public class Products {
     public void setPname(String pname) {
         this.pname = pname;
     }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public long getPrice() {
+        return price;
+    }
+
+    public void setPrice(long price) {
+        this.price = price;
+    }
+
 }
